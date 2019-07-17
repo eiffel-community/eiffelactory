51b35ad22ca40f27677f1fca7400938732126576
Alena Hramyka <alenah@axis.com>
2019-07-17 16:31:59 +0200

    rabbitmq.py: Update module to query Artifactory
    It now uses the artifactory module to find the artifact from the
    received ArtC events. Additionally, converted constants to uppercase
    after pylinting and added docstrings.
    
    Change-Id: I13c70aeea59e8c4c13a8072fcf36d9f9542ef1b1

--- eiffelactory/rabbitmq.py
+++ eiffelactory/rabbitmq.py
@@ -1,24 +1,29 @@
+"""
+Module for sending and receiving messages from RabbitMQ.
+"""
 import configparser
 import signal
 import sys
 
 from kombu import Connection, Exchange, Queue
 
-cfg = configparser.ConfigParser()
-cfg.read('../rabbitmq.config')
+from artifactory import find_artifact
 
-username = cfg.get('rabbitmq', 'username')
-password = cfg.get('rabbitmq', 'password')
-host = cfg.get('rabbitmq', 'host')
-port = int(cfg.get('rabbitmq', 'port'))
-vhost = cfg.get('rabbitmq', 'vhost')
-exchange = cfg.get('rabbitmq', 'exchange')
-exchange_type = cfg.get('rabbitmq', 'exchange_type')
-routing_key = cfg.get('rabbitmq', 'routing_key')
-queue = cfg.get('rabbitmq', 'queue')
+CONFIG = configparser.ConfigParser()
+CONFIG.read('../rabbitmq.config')
 
-eiffel_exchange = Exchange(exchange)
-eiffel_queue = Queue(queue, routing_key=routing_key)
+USERNAME = CONFIG.get('rabbitmq', 'username')
+PASSWORD = CONFIG.get('rabbitmq', 'password')
+HOST = CONFIG.get('rabbitmq', 'host')
+PORT = int(CONFIG.get('rabbitmq', 'port'))
+VHOST = CONFIG.get('rabbitmq', 'vhost')
+EXCHANGE = CONFIG.get('rabbitmq', 'exchange')
+EXCHANGE_TYPE = CONFIG.get('rabbitmq', 'exchange_type')
+ROUTING_KEY = CONFIG.get('rabbitmq', 'routing_key')
+QUEUE = CONFIG.get('rabbitmq', 'queue')
+
+EIFFEL_EXCHANGE = Exchange(EXCHANGE)
+EIFFEL_QUEUE = Queue(QUEUE, routing_key=ROUTING_KEY)
 
 
 class RabbitMQConnection:
@@ -27,16 +32,16 @@ class RabbitMQConnection:
     """
     def __init__(self):
         self.connection = Connection(transport='amqp',
-                                     hostname=host,
-                                     port=port,
-                                     userid=username,
-                                     password=password,
-                                     virtual_host=vhost,
+                                     hostname=HOST,
+                                     port=PORT,
+                                     userid=USERNAME,
+                                     password=PASSWORD,
+                                     virtual_host=VHOST,
                                      ssl=True)
         self.connection.connect()
         self.producer = self.connection.Producer(serializer='json')
         self.consumer = self.connection.Consumer(
-            eiffel_queue,
+            EIFFEL_QUEUE,
             callbacks=[self.process_message])
         self.consuming = True
         signal.signal(signal.SIGINT, self.signal_handler)
@@ -48,8 +53,7 @@ class RabbitMQConnection:
         :param message:
         :return:
         """
-        print(body)
-        print(message)
+        find_artifact(body)
         message.ack()
 
     def publish_message(self, message):
@@ -66,8 +70,8 @@ class RabbitMQConnection:
                                   'interval_max': 30,
                                   'max_retries': 30,
                               },
-                              exchange=eiffel_exchange,
-                              routing_key=routing_key)
+                              exchange=EIFFEL_EXCHANGE,
+                              routing_key=ROUTING_KEY)
 
     def read_messages(self):
         """
@@ -90,6 +94,13 @@ class RabbitMQConnection:
         self.connection.release()
 
     def signal_handler(self, signal_received, frame):
+        """
+        Method for handling Ctrl-C. The two unused arguments have to be there,
+        otherwise it won't work
+        :param signal_received:
+        :param frame:
+        :return:
+        """
         # closes down everything when Ctrl-C is pressed
         self.close_connection()
         print("\nExiting")
@@ -99,5 +110,4 @@ class RabbitMQConnection:
 if __name__ == "__main__":
     # this is for easy testing
     mq = RabbitMQConnection()
-    mq.publish_message("Test Eiffelactory")
     mq.read_messages()
