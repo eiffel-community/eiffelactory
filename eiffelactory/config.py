ab7d5a0f5d21aeca47f39ba0e029409eea263f38
Martin Winqvist <martinwi@axis.com>
2019-07-22 15:57:57 +0200

    bugfix:  parsing event_sources did not split on ','
    added and clearified comments

--- eiffelactory/config.py
+++ eiffelactory/config.py
@@ -1,5 +1,12 @@
 """
-Config module for prettier logging across the classes in the app
+Config module for handling the eiffelactory.config file.
+Wraps the different sections in the config file in their own classes
+and exposes options as object properties in order to take advantage of
+dot.notation and auto-completion.
+
+This allows for safer and easier access of config options.
+
+Parses option strings into collections or other formats if needed.
 """
 import configparser
 
@@ -13,10 +20,10 @@ CONFIG_EIFFELACTORY = CONFIG['eiffelactory']
 
 class Config:
     """
-    for prettier logging across the classes in the app
+    Holds all the different config sections.
     """
+
     def __init__(self):
-        self.config = CONFIG
         self.rabbitmq = RabbitMQConfig()
         self.artifactory = ArtifactoryConfig()
         self.eiffelactory = EiffelactoryConfig()
@@ -25,8 +32,14 @@ class Config:
 class ConfigSection:
     """
     Wrapper parent class for easier and safer extraction of configs from
-    the config file
+    the config file. Wraps a single config section.
+
+    Strings always fallback to empty strings
+    while int and booleans always fallback to None.
+
+    :param section: the config section
     """
+
     def __init__(self, section):
         self.section = section
 
@@ -42,9 +55,10 @@ class ConfigSection:
 
 class RabbitMQConfig(ConfigSection):
     """
-    Child of ConfigSection for easier and safer extraction of RabbitMQ-related
-    configs from the config file
+    Wraps the rabbitmq section of the config file for easier and safer
+    extraction of RabbitMQ related options.
     """
+
     DEFAULT_ROUTING_KEY = '#'
     DEFAULT_VHOST = '/'
     DEFAULT_PREFETCH_COUNT = '50'
@@ -95,9 +109,10 @@ class RabbitMQConfig(ConfigSection):
 
 class ArtifactoryConfig(ConfigSection):
     """
-    Child of ConfigSection for easier and safer extraction of Artifactory-
-    related configs from the config file
+    Wraps the artifactory section of the config file for easier and safer
+    extraction of artifactory related options.
     """
+
     def __init__(self):
         super().__init__(CONFIG_ARTIFACTORY)
 
@@ -116,12 +131,13 @@ class ArtifactoryConfig(ConfigSection):
 
 class EiffelactoryConfig(ConfigSection):
     """
-    Child of ConfigSection for easier and safer extraction of related
-    configs specifically for this plugin from the config file
+    Wraps the eiffelactory section of the config file for easier and safer
+    extraction of app related options.
     """
+
     def __init__(self):
         super().__init__(CONFIG_EIFFELACTORY)
 
     @property
     def event_sources(self):
-        return self.get('event_sources').replace(' ', '').split()
+        return self.get('event_sources').replace(' ', '').split(',')
