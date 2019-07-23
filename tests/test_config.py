786f76d3554c68aa05b10541719051fbaccd9493
Martin Winqvist <winqvist.martin@gmail.com>
2019-07-23 18:48:04 +0200

    added unit tests for config

---
+++ tests/test_config.py
@@ -0,0 +1,54 @@
+import unittest
+import configparser
+from eiffelactory import config
+
+
+class ConfigTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.config = config.Config("eiffelactory.config.test")
+        self.no_default_options = config.Config(
+            'no_default_options.config.test')
+
+    def test_option_event_sources_should_be_parsed_to_list(self):
+        event_sources = self.config.eiffelactory.event_sources
+
+        self.assertIs(type(event_sources), list)
+        self.assertEqual(len(event_sources), 3)
+        self.assertEqual(event_sources[0], "JENKINS_EIFFEL_BROADCASTER")
+        self.assertEqual(event_sources[1], "EVENT-source_2")
+        self.assertEqual(event_sources[2], "eventSource3")
+
+    def test_missing_event_sources_should_be_empty_list(self):
+        event_sources = self.no_default_options.eiffelactory.event_sources
+
+        self.assertIs(event_sources, None)
+
+    def test_returns_default_values_if_option_is_missing(self):
+        cfg = self.no_default_options
+        defaults = config.DEFAULT_CONFIG_OPTIONS
+
+        self.assertIs(cfg.rabbitmq.prefetch_count,
+                      int(defaults['prefetch_count']))
+
+        self.assertIs(cfg.rabbitmq.vhost, defaults['vhost'])
+        self.assertIs(cfg.rabbitmq.routing_key, defaults['routing_key'])
+        self.assertIs(cfg.eiffelactory.event_sources, defaults['event_sources'])
+
+    def test_missing_sections_are_added(self):
+        cfg = self.no_default_options
+        self.assertTrue(cfg._config.has_section('eiffelactory'))
+
+    def test_port_is_int(self):
+        port = self.config.rabbitmq.port
+
+        self.assertIs(type(port), int)
+
+    def test_prefetch_count_is_int(self):
+        prefetch_count = self.config.rabbitmq.prefetch_count
+
+        self.assertIs(type(prefetch_count), int)
+
+    def tearDown(self):
+        self.config = None
+        self.no_default_options = None
