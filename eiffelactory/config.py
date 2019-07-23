10b6b322c253d9d708e91d8af4ad3caa5980a91e
Martin Winqvist <winqvist.martin@gmail.com>
2019-07-23 18:47:16 +0200

    refactored config for easier testing and usage

--- eiffelactory/config.py
+++ eiffelactory/config.py
@@ -2,7 +2,7 @@
 Config module for handling the eiffelactory.config file.
 Wraps the different sections in the config file in their own classes
 and exposes options as object properties in order to take advantage of
-dot.notation and auto-completion.
+dot.notation.
 
 This allows for safer and easier access of config options.
 
@@ -10,23 +10,27 @@ Parses option strings into collections or other formats if needed.
 """
 import configparser
 
-CONFIG = configparser.ConfigParser()
-CONFIG.read('eiffelactory.config')
+DEFAULT_CONFIG_FILENAME = 'eiffelactory.config'
 
-CONFIG_RABBITMQ = CONFIG['rabbitmq']
-CONFIG_ARTIFACTORY = CONFIG['artifactory']
-CONFIG_EIFFELACTORY = CONFIG['eiffelactory']
-
+DEFAULT_CONFIG_OPTIONS = {
+    'prefetch_count': '50',
+    'vhost': '/',
+    'routing_key': '#',
+    'event_sources': None
+}
 
 class Config:
     """
     Holds all the different config sections.
     """
 
-    def __init__(self):
-        self.rabbitmq = RabbitMQConfig()
-        self.artifactory = ArtifactoryConfig()
-        self.eiffelactory = EiffelactoryConfig()
+    def __init__(self, filename=DEFAULT_CONFIG_FILENAME):
+        self._config = configparser.ConfigParser(defaults=DEFAULT_CONFIG_OPTIONS)
+        self._config.read(filename)
+
+        self.rabbitmq = RabbitMQConfig(self._config, 'rabbitmq')
+        self.artifactory = ArtifactoryConfig(self._config, 'artifactory')
+        self.eiffelactory = EiffelactoryConfig(self._config, 'eiffelactory')
 
 
 class ConfigSection:
@@ -34,23 +38,24 @@ class ConfigSection:
     Wrapper parent class for easier and safer extraction of configs from
     the config file. Wraps a single config section.
 
-    Strings always fallback to empty strings
-    while int and booleans always fallback to None.
-
     :param section: the config section
     """
 
-    def __init__(self, section):
-        self.section = section
+    def __init__(self, config, section):
+        self._config = config
+        self._section = section
 
-    def get(self, key, default=None):
-        return self.section.get(key, fallback=default)
+        if not self._config.has_section(section):
+            self._config.add_section(section)
 
-    def getint(self, key, default=None):
-        return self.section.getint(key, fallback=default)
+    def get(self, key):
+        return self._config.get(self._section, key)
 
-    def getbool(self, key, default=None):
-        return self.section.getboolean(key, fallback=default)
+    def getint(self, key):
+        return self._config.getint(self._section, key)
+
+    def getboolean(self, key):
+        return self._config.getboolean(self._section, key)
 
 
 class RabbitMQConfig(ConfigSection):
@@ -59,12 +64,8 @@ class RabbitMQConfig(ConfigSection):
     extraction of RabbitMQ related options.
     """
 
-    DEFAULT_ROUTING_KEY = '#'
-    DEFAULT_VHOST = '/'
-    DEFAULT_PREFETCH_COUNT = '50'
-
-    def __init__(self):
-        super().__init__(CONFIG_RABBITMQ)
+    def __init__(self, config, section):
+        super().__init__(config, section)
 
     @property
     def username(self):
@@ -80,7 +81,7 @@ class RabbitMQConfig(ConfigSection):
 
     @property
     def port(self):
-        return self.get('port')
+        return self.getint('port')
 
     @property
     def exchange(self):
@@ -96,15 +97,15 @@ class RabbitMQConfig(ConfigSection):
 
     @property
     def routing_key(self):
-        return self.get('routing_key', self.DEFAULT_ROUTING_KEY)
+        return self.get('routing_key')
 
     @property
     def vhost(self):
-        return self.get('vhost', self.DEFAULT_VHOST)
+        return self.get('vhost')
 
     @property
     def prefetch_count(self):
-        return self.getint('prefetch_count', self.DEFAULT_PREFETCH_COUNT)
+        return self.getint('prefetch_count')
 
 
 class ArtifactoryConfig(ConfigSection):
@@ -113,8 +114,8 @@ class ArtifactoryConfig(ConfigSection):
     extraction of artifactory related options.
     """
 
-    def __init__(self):
-        super().__init__(CONFIG_ARTIFACTORY)
+    def __init__(self, config, section):
+        super().__init__(config, section)
 
     @property
     def username(self):
@@ -139,8 +140,8 @@ class EiffelactoryConfig(ConfigSection):
     extraction of app related options.
     """
 
-    def __init__(self):
-        super().__init__(CONFIG_EIFFELACTORY)
+    def __init__(self, config, section):
+        super().__init__(config, section)
 
     @property
     def event_sources(self):
