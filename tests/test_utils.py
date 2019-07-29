f549a67b72fe28acf26f3f8f6fed698601930903
Alena Hramyka <alegroma@gmail.com>
2019-07-29 13:19:31 +0200

    Modify utils unittest after Eiffel Broadcaster upd
    The format of the purl included in ArtC events sent by Jenkins Eiffel
    Broadcaster has been changed, hence update the unit test accordingly.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -16,14 +16,15 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(clean, expected)
 
     def test_parse_identity_purl(self):
-        purl = 'pkg:job/DEPT/job/USR/job/TEST/job/FOO/job/BAR_BAR/1234/' \
-               'artifacts/some_file.txt@1234'
+        purl = 'pkg:artifacts/some_file.txt@1234?build_path=' \
+               'job/DEPT/job/USR/job/TEST/job/FOO/job/BAR_BAR/1234/' \
+
 
         filename, build_url = utils.parse_purl(purl)
 
         self.assertEqual(filename, 'some_file.txt')
         self.assertEqual(build_url,
-                'job/DEPT/job/USR/job/TEST/job/FOO/job/BAR_BAR/1234')
+                'job/DEPT/job/USR/job/TEST/job/FOO/job/BAR_BAR/1234/')
 
 
 if __name__ == '__main__':
